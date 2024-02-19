77b5949c46cd06d6f9576e2d4e7981971dea56b6
Samjhana Chaudhary <samchau78@gmail.com>
2024-02-19 08:02:49 +0545

    Initial Commit

---
+++ IndexerDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerDemo
+{
+    internal class Program
+    {
+        // declare an array to store elements  
+        private string[] studentName = new string[10];
+        // define an indexer
+        public string this[int index]
+        {
+            get
+            {
+                // return value of stored at studentName array  
+                return studentName[index];
+            }
+
+            set
+            {
+                // assigns value to studentName  
+                studentName[index] = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // create instance of Program class 
+            Program obj = new Program();
+
+            // insert values in obj[] using indexer i.e index position
+            obj[0] = "Harry";
+            obj[1] = "Ron";
+            obj[2] = "Hermoine";
+
+            Console.WriteLine("First element in obj: " + obj[0]);
+            Console.WriteLine("Second element in obj: " + obj[1]);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ StaticDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StaticDemo
+{
+   
+        class MyCollege
+        {
+            //static fields
+            public static string CollegeName;
+            public static string Address;
+
+            //static constructor
+            static MyCollege()
+            {
+                CollegeName = "ABC College of Technology";
+                Address = "Hyderabad";
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                 
+                Console.WriteLine(MyCollege.CollegeName);
+                Console.WriteLine(MyCollege.Address);
+                Console.ReadLine();
+            }
+        }
+    }
+
